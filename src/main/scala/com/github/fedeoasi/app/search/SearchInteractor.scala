0bd9c5392a23f362c4f42f9ba38ca6006f21c952
Federico Caimi <fedeoasi@gmail.com>
2013-06-30 00:20:07 -0500

    Index an entire srt file in elasticsearch.

--- src/main/scala/com/github/fedeoasi/app/search/SearchInteractor.scala
+++ src/main/scala/com/github/fedeoasi/app/search/SearchInteractor.scala
@@ -11,17 +11,20 @@ import org.json4s.jackson.JsonMethods._
 import java.text.SimpleDateFormat
 import org.elasticsearch.action.search.SearchType
 import org.elasticsearch.index.query.QueryBuilders
+import scala.collection.JavaConversions._
 
 trait SearchInteractor {
   def ensureIndexExists(name: String): Boolean
-  def indexSubtitleEntry(index: String, entry: SubEntry, subtitleId: String, imdbId: String): Boolean
-  def searchSubtitles(index: String, query: String): List[SearchResult]
+  def indexSubtitleEntry(index: String, entry: SubEntry, subtitleId: String, imdbId: String, flush: Boolean): Boolean
+  def indexSubtitleEntries(index: String, entries: List[SubEntry], s1: String, s2: String): Boolean
+  def searchSubtitles(index: String, query: String): List[SubEntry]
+  def getSubtitleEntries(index: String, ids: List[String]): List[SubEntry]
   def deleteIndex(name: String)
   def close()
 }
 
 class ElasticSearchInteractor extends SearchInteractor{
-  val node = nodeBuilder().clusterName("lbs").node()
+  val node = nodeBuilder().client(false).clusterName("lbs").node()
   var client = node.client()
   val format = new SimpleDateFormat("HH:mm:ss,SSS")
 
@@ -43,31 +46,48 @@ class ElasticSearchInteractor extends SearchInteractor{
     createIndexResponse.isAcknowledged
   }
 
-  def indexSubtitleEntry(index: String, entry: SubEntry, subtitleId: String, imdbId: String): Boolean = {
+  def indexSubtitleEntry(index: String, entry: SubEntry, subtitleId: String, movieId: String, flush: Boolean): Boolean = {
     val json = (
       ("number" -> entry.number) ~
       ("start" -> format.format(entry.start)) ~
       ("stop" -> format.format(entry.stop)) ~
       ("text" -> entry.text) ~
       ("subtitleId" -> subtitleId) ~
-      ("imdbId" -> imdbId)
+      ("imdbId" -> movieId)
     )
     val response = client.prepareIndex(index, "entry")
       .setSource(compact(render(json)))
       .execute()
       .actionGet();
-    val flushResponse = client.admin().indices().prepareFlush(index).execute().actionGet()
+    if(flush) {
+      client.admin().indices().prepareFlush(index).execute().actionGet()
+    }
     response.getId != null
   }
 
-  def searchSubtitles(index: String, query: String): List[SearchResult] = {
+  def indexSubtitleEntries(index: String, entries: List[SubEntry], subtitleId: String, movieId: String): Boolean = {
+    entries.foreach(
+      e => indexSubtitleEntry(index, e, subtitleId, movieId, false)
+    )
+    client.admin().indices().prepareFlush(index).execute().actionGet()
+    true
+  }
+
+  def searchSubtitles(index: String, query: String): List[SubEntry] = {
     val response = client.prepareSearch(index)
       .setTypes("entry")
       .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
-      .setQuery(QueryBuilders.termQuery("content", query))
+      .setQuery(QueryBuilders.termQuery("text", query))
       .execute()
-      .actionGet();
-    List[SearchResult]()
+      .actionGet()
+    val ids = response.getHits.map(
+      hit => hit.getId
+    )
+    if(ids.isEmpty) {
+      List[SubEntry]()
+    } else {
+      getSubtitleEntries(index, ids.toList).toList
+    }
   }
 
   def close() {
@@ -79,6 +99,21 @@ class ElasticSearchInteractor extends SearchInteractor{
     val deleteIndexBuilder = indicesClient.prepareDelete(name);
     val deleteIndexResponse = deleteIndexBuilder.execute().actionGet();
     deleteIndexResponse.isAcknowledged
+  }
+
+  def getSubtitleEntries(index: String, ids: List[String]): List[SubEntry] = {
+    val response = client.prepareMultiGet().add(index, "entry", ids).execute().actionGet()
+    val entries = response.iterator().map(
+      r => mapToSubtitleEntry(r.getResponse.getSourceAsMap())
+    )
+    entries.toList
+  }
+
+  def mapToSubtitleEntry(entryMap: java.util.Map[String, Object]): SubEntry = {
+    SubEntry(entryMap.get("number").asInstanceOf[Int],
+      format.parse(entryMap.get("start").asInstanceOf[String]),
+      format.parse(entryMap.get("stop").asInstanceOf[String]),
+      entryMap.get("text").asInstanceOf[String])
   }
 }
 
--- src/test/scala/com/github/fedeoasi/app/search/ElasticSearchSpec.scala
+++ src/test/scala/com/github/fedeoasi/app/search/ElasticSearchSpec.scala
@@ -3,16 +3,16 @@ package com.github.fedeoasi.app.search
 import org.scalatest.FunSpec
 import org.scalatest.matchers.ShouldMatchers
 import org.elasticsearch.node.NodeBuilder._
-import org.elasticsearch.common.settings.ImmutableSettings
-import org.elasticsearch.client.IndicesAdminClient
 import java.util.UUID
 import com.github.fedeoasi.app.model.SubEntry
 import java.text.SimpleDateFormat
+import java.io.FileInputStream
+import com.github.fedeoasi.app.parsing.SrtParser
 
 class ElasticSearchSpec extends FunSpec with ShouldMatchers {
   describe("Elastic Search") {
     ignore("should be able to join the cluster") {
-      val node = nodeBuilder().clusterName("lbs").node()
+      val node = nodeBuilder().client(false).clusterName("lbs").node()
       val client = node.client()
       client.close()
     }
@@ -34,10 +34,11 @@ class ElasticSearchSpec extends FunSpec with ShouldMatchers {
     it("should be able to index an entry of a subtitle file") {
       val interactor = new ElasticSearchInteractor()
       val format = new SimpleDateFormat("HH:mm:ss,SSS")
-      val name: String = "test-index" + UUID.randomUUID()
+      val name: String = UUID.randomUUID() + "test-index"
+      println("Cluster name: " + name)
       interactor.ensureIndexExists(name)
       val entry: SubEntry = SubEntry(1, format.parse("00:15:11,123"), format.parse("00:16:11,123"), "This is the content")
-      interactor.indexSubtitleEntry(name, entry, "subId", "imdbId")
+      interactor.indexSubtitleEntry(name, entry, "subId", "imdbId", true)
       val results = interactor.searchSubtitles(name, "content")
       interactor.deleteIndex(name)
       interactor.close()
@@ -45,7 +46,20 @@ class ElasticSearchSpec extends FunSpec with ShouldMatchers {
     }
 
     it("should be able to index an entire subtitle file") {
+      val interactor = new ElasticSearchInteractor()
+      val format = new SimpleDateFormat("HH:mm:ss,SSS")
+      val name: String = UUID.randomUUID() + "test-index"
+      println("Cluster name: " + name)
+      interactor.ensureIndexExists(name)
 
+      val input = new FileInputStream("resources/4899518.srt")
+      val entries = new SrtParser().parseSrt(input)
+
+      interactor.indexSubtitleEntries(name, entries, "subId", "imdbId")
+      val results = interactor.searchSubtitles(name, "content")
+      interactor.deleteIndex(name)
+      interactor.close()
+      results.size should be(0)
     }
   }
 }
